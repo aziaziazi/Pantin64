4b2ac802c98f28bc4a2177747724cbd2df75b1fd
aziaziazi <camille@MBP-de-Camille.lan>
2016-03-25 10:33:51 +0100

    Server support of device orientation
    To slow now. Have to check if problem in script, connexion or server.

--- Pantin64_S_0.5/gamepad.js
+++ Pantin64_S_0.5/gamepad.js
@@ -1,12 +1,86 @@
 var socket = io();
 
+// true = Landscape
+// false = portrait
+// var land
+
+// function checkOr(){
+//     if(window.innerHeight > window.innerWidth){
+//       land = false;
+//       socket.emit("land", land)
+//     }else{
+//       land = true;
+//       socket.emit("land", land)
+//     };
+// };
+
+
+// function getRawOrientation(event){
+//   var beta = event.beta;
+//   var gamma = event.gamma;
+//   return {"beta":beta,
+//           "gamma":gamma}
+// };
+
 function initGamePad(){
+
+  // IMPLEMENT: check device compatibility
+
+  // At orientation change, check new orientation
+  // window.addEventListener("orientationchange", function(){
+  //   checkOr()
+  // });
+
+  // // TODO: Optimise : is there a way not to check land each time ?
+  // window.addEventListener("deviceorientation", function(e){
+  //   if (land == true){
+  //     var rowRot = -getRawOrientation(e).beta
+  //   }else{
+  //     var rowRot = getRawOrientation(e).gamma
+  //   }
+
+  //   // TODO:update rowrot instead of creating rot ?
+  //   var rot = Math.round(rowRot);
+
+    // TOTO: optimise
+    // Send Right(t) or Left(y)
+
+    // if (rot>5){
+    //   socket.emit("dir", "l");
+    // } else if (-rot>5){
+    //   socket.emit("dir", "r");
+    // }else{
+    //   socket.emit("dir", "x");
+    //   socket.emit("left_up");
+    // }
+
+
+    // BEFORE Optimisation 1
+    //     if (rot>5){
+    //   socket.emit("left_up");
+    //   socket.emit("right_do");
+    //   // socket.emit("orientation", "Right")
+    // } else if (-rot>5){
+    //   socket.emit("right_up");
+    //   socket.emit("left_do");
+    //   // socket.emit("orientation", "left")
+
+    // }else{
+    //   socket.emit("right_up");
+    //   socket.emit("left_up");
+    // }
+
+    // socket.emit("orientation", rot)
+  // });
 
   var but_j = document.getElementById('j')
   var but_g = document.getElementById('g')
   var but_f = document.getElementById('f')
   var but_b = document.getElementById('b')
 
+
+  // TODO: Loop functions (?)
+  // TOTO: Use Json (?)
   but_j.addEventListener("touchstart", function(e){
     socket.emit('j_do');
   }, false);
--- Pantin64_S_0.5/server.js
+++ Pantin64_S_0.5/server.js
@@ -20,56 +20,76 @@ app.get("/", function(req,res){
 io.on('connection', function(socket){
 	console.log("socket ON")
 
- //  socket.on(keyMap["A button"].domId, function(){
-	//   	// console.log(keyMap["A button"].Action)
-	// 	  // robot.keyTap(keyMap["A button"].computerKey.p1);
-	// 	  robot.keyToggle("a", "down");
- //  });
- //  socket.on(keyMap["B button"].domId, function(){
-	//   	// console.log(keyMap["B button"].Action)
-	// 	  // robot.keyTap(keyMap["B button"].computerKey.p1);
-	// 	  robot.keyTap("z");
- //  });
- //  socket.on(keyMap["R button"].domId, function(){
-	//   	// console.log(keyMap["R button"].Action)
-	// 	  // robot.keyTap(keyMap["R button"].computerKey.p1);
-	// 	  robot.keyTap("e");
- //  });
- //  socket.on(keyMap["Z button"].domId, function(){
-	//   	// console.log(keyMap["Z button"].Action)
-	// 	  // robot.keyTap(keyMap["Z button"].computerKey.p1);
-	// 	  robot.keyTap("r");
+	socket.on("land", function(isTrue){
+		console.log(isTrue)
+	});
+
+	socket.on("orientation", function(log){
+		console.log(log);
+	});
+
+
+	// TOTO: Loop functions (?)
+	// TODO: Use JSON (?)
+	socket.on("j_do", function(){
+		robot.keyToggle("e", "down");
+	});
+	socket.on("j_up", function(){
+		robot.keyToggle("e", "up");
+	});
+
+	socket.on("g_do", function(){
+		robot.keyToggle("a", "down");
+	});
+	socket.on("g_up", function(){
+		robot.keyToggle("a", "up");
+	});
+
+	socket.on("f_do", function(){
+		robot.keyToggle("r", "down");
+	});
+	socket.on("f_up", function(){
+		robot.keyToggle("r", "up");
+	});
+
+	socket.on("b_do", function(){
+		robot.keyToggle("z", "down");
+	});
+	socket.on("b_up", function(){
+		robot.keyToggle("z", "up");
+	});
+
+	// socket.on("right_do", function(){
+	// 	robot.keyToggle("y", "down");
+	// });
+	// socket.on("right_up", function(){
+	// 	robot.keyToggle("y", "up");
 	// });
 
+	// socket.on("left_do", function(){
+	// 	robot.keyToggle("t", "down");
+	// });
+	// socket.on("left_up", function(){
+	// 	robot.keyToggle("t", "up");
+	// });
 
-  socket.on("j_do", function(){
-	  robot.keyToggle("e", "down");
-	});
-  socket.on("j_up", function(){
-	  robot.keyToggle("e", "up");
-	});
+	var y_up = function(){robot.keyToggle("y", "up")};
+	var t_up = function(){robot.keyToggle("t", "up")};
 
-	  socket.on("g_do", function(){
-	  robot.keyToggle("a", "down");
-	});
-  socket.on("g_up", function(){
-	  robot.keyToggle("a", "up");
-	});
+	socket.on("dir", function(d){
+		if (d=="l"){
+			robot.keyToggle("y", "down");
+			// setTimeout(y_up, 100);
+		}else if (d=="r"){
+			robot.keyToggle("t", "down")
+			// setTimeout(t_up, 100);
 
-	  socket.on("f_do", function(){
-	  robot.keyToggle("r", "down");
+		}
+		else if (d=="x"){
+			robot.keyToggle("y", "up")
+			robot.keyToggle("t", "up")
+		}
 	});
-  socket.on("f_up", function(){
-	  robot.keyToggle("r", "up");
-	});
-
-	  socket.on("b_do", function(){
-	  robot.keyToggle("z", "down");
-	});
-  socket.on("b_up", function(){
-	  robot.keyToggle("z", "up");
-	});
-
 });
 
 http.listen(port, hostname, function(){
